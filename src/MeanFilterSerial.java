e05523567a9bacff7c7b6f065f4f406c99d91ea9
RacquelDennison <racquelnina@gmail.com>
2022-08-04 22:17:37 +0200

    created the array to read in the images

--- src/MeanFilterSerial.java
+++ src/MeanFilterSerial.java
@@ -8,21 +8,30 @@ import java.io.*;;
 
 public class MeanFilterSerial {
 
-    public static void main (String[] args) throws IOException {
-        try{
-        File imageFile = new File("minions.jfif");
-        BufferedImage image = ImageIO.read(imageFile);
-        //getting the dimensions of the image 
-        int maxHeight = image.getHeight();
-        int maxWidth =image.getWidth();
+    public static void main(String[] args) throws IOException {
+        int maxHeight = 0;
+        int maxWidth = 0;
+        File imageFile;
+        BufferedImage image = null;
+        try {
+            imageFile = new File("minions.jfif");
+            image = ImageIO.read(imageFile);
+            // getting the dimensions of the image
+            maxHeight = image.getHeight();
+            maxWidth = image.getWidth();
+        }
 
-        
+        catch (IOException e) {
+            System.out.println("Error was faced: " + e);
+        }
 
-    } 
-    catch (IOException e){
-        System.out.println("Error was faced: "+e);
-        
+        // inserting the coloration of each image pixel into the array
+        int picturefile[][] = new int[maxHeight][maxWidth];
+        for (int i = 0; i < maxHeight; i++) {
+            for (int j = 0; j < maxWidth; j++) {
+                picturefile[i][j] = image.getRGB(j, i);
+            }
+        }
 
     }
-
 }
