988d5e5051ef9414a966cdcef26097ba23a769ba
RacquelDennison <racquelnina@gmail.com>
2022-08-05 20:53:49 +0200

    working mean filter

--- src/MeanFilterSerial.java
+++ src/MeanFilterSerial.java
@@ -2,6 +2,7 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
+import javax.sound.midi.Sequencer.SyncMode;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.io.*;
@@ -11,97 +12,69 @@ import javax.swing.*;
 
 public class MeanFilterSerial {
 
-    public int average(ArrayList<Integer> array) {
-        int sum = 0;
-        for (int i = 0; i < array.size(); i++) {
-            sum += array.get(i);
+    // methods
+
+    public static int average(int x, int y, BufferedImage image, int radius) {
+        int totals[] = { 0, 0, 0 };
+        int count = 0;
+        for (int i = x; i < x + radius; i++) {
+            // add outter bounds conditions
+            for (int j = y - radius; j <= y + radius; ++j) {
+                int pixel = image.getRGB(i, j);
+                totals[0] += (pixel >> 16) & 0xff;
+                totals[1] += (pixel >> 8) & 0xff;
+                totals[2] += pixel & 0xff;
+                ++count;
+
+            }
 
         }
-        return sum / array.size();
+        return makePixel(totals[0] / count, totals[1] / count, totals[2] / count);
+
     }
 
+    public static int makePixel(int r, int g, int b) {
+        return (r << 16) | (g << 8) | b;
+    }
+
+    // main class
     public static void main(String[] args) throws IOException {
         int maxHeight = 0;
         int maxWidth = 0;
-        File imageFile;
+        File imageFile; // args[0]
         BufferedImage image = null;
-        int sliderVariable = 3;
-        int kernelWidth = sliderVariable;
-        int kernelHeight = sliderVariable;
-        int stoppingVariable = sliderVariable / 2;
-        int counterVariable = stoppingVariable;
-        // declaring the arrays to store the pixels
-        ArrayList<Integer> redPixels = new ArrayList<>();
-        ArrayList<Integer> bluePixels = new ArrayList<>();
-        ArrayList<Integer> greenPixels = new ArrayList<>();
-        boolean keepGoing = true;
-        MeanFilterSerial classObject = new MeanFilterSerial();
+        int sliderVariable = 3; // args[2]
+        int radius = sliderVariable / 2;
+        BufferedImage image2 = null; // args[1]
 
-        int output[][];
         try {
-            imageFile = new File("minions.jfif");
+            // set all the variabls of the file
+            imageFile = new File("noisy.png"); // TODO jpg
             image = ImageIO.read(imageFile);
             // getting the dimensions of the image
             maxHeight = image.getHeight();
             maxWidth = image.getWidth();
+            image2 = new BufferedImage(maxWidth, maxHeight, BufferedImage.TYPE_INT_RGB);
+            // new BufferedImage(maxWidth, maxHeight, BufferedImage.TYPE_INT_RGB);
         }
 
         catch (IOException e) {
             System.out.println("Error was faced: " + e);
         }
-
-        // inserting the coloration of each image pixel into the array
-        // int picturefile[][] = new int[maxHeight][maxWidth];
-        // for (int i = 0; i < maxHeight; i++) {
-        // for (int j = 0; j < maxWidth; j++) {
-        // picturefile[i][j] = image.getRGB(j, i);
-        // // System.out.println(image.getRGB(j, i));
-
-        // }
-        // }
         // TODO Fix the looping variables
-        for (int v = stoppingVariable; v < maxHeight - stoppingVariable; v++) {
-            for (int u = stoppingVariable; u < maxWidth - stoppingVariable; u++) {
-                for (int p = v - stoppingVariable; p < kernelHeight; p++) {
-                    for (int q = u - stoppingVariable; q < kernelWidth; q++) {
-                        // Color color = new Color(image.getRGB(p, q)); // getting the color at each
-                        // // pixel
-                        int pixelValue = image.getRGB(p, q);
-                        int redValue = (pixelValue >> 16) & 0xff;
-                        int blueValue = pixelValue & 0xff;
-                        int greenValue = (p >> 8) & 0xff;
-                        redPixels.add(redValue);
-                        bluePixels.add(blueValue);
-                        greenPixels.add(greenValue);
-                    }
 
-                }
-                int redAverage = classObject.average(redPixels);
-                int blueAverage = classObject.average(bluePixels);
-                int greenAverage = classObject.average(greenPixels);
-                int p = (redAverage + blueAverage + greenAverage) / 3;
-                image.setRGB(v, u, p);
-
+        for (int x = radius; x < maxWidth - radius; x++) {
+            for (int y = radius; y < maxHeight - radius; y++) {
+                // call an average method
+                image2.setRGB(x, y, average(x, y, image, radius));
             }
+
         }
-        // TODO see how to insert the image
-        // re-inserting the image
-
-        // BufferedImage theImage = new BufferedImage(
-        // maxHeight,
-        // maxWidth,
-        // BufferedImage.TYPE_INT_RGB);
-        // int value;
-        // for (int y = 1; y < maxHeight; y++) {
-        // for (int x = 1; x < maxWidth; x++) {
-        // value = picturefile[y][x];
-        // theImage.setRGB(y, x, value);
-        // }
-        // }
-
-        // File outputfile = new File("task1output3x3.png");
+        File outputfile = new File("task1output3x3.png");
 
         // TODO- maybe just writing to the thing will work
-        // ImageIO.write(theImage, "png", outputfile);
+        ImageIO.write(image2, "jpg", outputfile);
+
     }
+
 }
