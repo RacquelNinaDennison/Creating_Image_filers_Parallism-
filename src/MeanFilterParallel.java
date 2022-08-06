a62ea3d07030a5b067441c39d5ce3dfafc7da1a7
RacquelDennison <racquelnina@gmail.com>
2022-08-06 19:42:41 +0200

    finished parallel

--- src/MeanFilterParallel.java
+++ src/MeanFilterParallel.java
@@ -1,27 +1,20 @@
-import java.awt.Graphics;
-import java.awt.Image;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
-import javax.sound.midi.Sequencer.SyncMode;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
 import java.io.*;
-import java.util.*;
-import java.awt.*;
-import javax.swing.*;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveAction;
 
 public class MeanFilterParallel extends RecursiveAction {
-    int height;
-    int width;
+
     static File imageFile; // args[0]
     static BufferedImage image = null; // buffered image to send to
-    static int sliderVariable; // args[2]
+    static int sliderVariable = 9; // args[2]
     static int radius = sliderVariable / 2;
     static BufferedImage image2 = null; // args[1]
-    protected static int sThreshold = 100000;
+    protected static int sThreshold = 100;
     int start;
+    static int height;
+    int width;
 
     // constructor method
     public MeanFilterParallel(int width, int start) {
@@ -31,12 +24,12 @@ public class MeanFilterParallel extends RecursiveAction {
     }
 
     protected void computeDirectly() {
-        for (int x = radius; x < start + radius; ++x) {
-            for (int y = 0; y < radius; ++y) {
+        for (int x = start; x < start + width; ++x) {
+            for (int y = 0; y < height; ++y) {
                 // call an average method
-                if (y < 0 || y >= image.getHeight()) {
-                    continue;
-                }
+                // if (y < 0 || y >= image.getHeight()) {
+                // continue;
+                // }
                 // System.out.println("Called the computeDirectly");
                 image2.setRGB(x, y, average(x, y, image, radius));
             }
@@ -62,58 +55,19 @@ public class MeanFilterParallel extends RecursiveAction {
 
     }
 
-    // main class
-    public static void main(String[] args) throws IOException {
-        try {
-            // set all the variabls of the file
-
-            imageFile = new File("example.jpg"); // TODO change it to the args
-            image = ImageIO.read(imageFile);
-            // getting the dimensions of the image
-
-        }
-
-        catch (IOException e) {
-            System.out.println("Error was faced: " + e);
-        }
-        // TODO Fix the looping variables
-        // this will be done once all the threads are done
-        int processors = Runtime.getRuntime().availableProcessors();
-        System.out.println(Integer.toString(processors) + " processor"
-                + (processors != 1 ? "s are " : " is ")
-                + "available");
-
-        MeanFilterParallel fb = new MeanFilterParallel(image.getWidth(), 0);
-        fb.height = image.getHeight();
-        image2 = new BufferedImage(fb.width, fb.height, BufferedImage.TYPE_INT_RGB);
-        mean(image);
-        // ForkJoinPool pool = new ForkJoinPool();
-        // long startTime = System.currentTimeMillis();
-        // pool.invoke(fb);
-        // long endTime = System.currentTimeMillis();
-
-        // System.out.println("Image blur took " + (endTime - startTime) +
-        // " milliseconds.");
-
-        System.out.println("Writing to file");
-        File outputfile = new File("imageBlurParallel.png");
-
-        // TODO- maybe just writing to the thing will work
-        ImageIO.write(image2, "jpg", outputfile);
-        System.out.println("Image done ");
-
-    }
-
-    public static void mean(BufferedImage image) {
-
-    }
-
     public static int average(int x, int y, BufferedImage image, int radius) {
         int totals[] = { 0, 0, 0 };
         int count = 0;
         for (int i = x; i < x + radius; i++) {
             // add outter bounds conditions
+            if (i < 0 || i >= image.getWidth()) {
+                continue;
+            }
+
             for (int j = y - radius; j <= y + radius; ++j) {
+                if (j < 0 || j >= image.getHeight()) {
+                    continue;
+                }
                 int pixel = image.getRGB(i, j);
                 totals[0] += (pixel >> 16) & 0xff;
                 totals[1] += (pixel >> 8) & 0xff;
@@ -123,14 +77,36 @@ public class MeanFilterParallel extends RecursiveAction {
             }
 
         }
-        System.out.println("Called the average filter");
-        return makePixel(totals[0] / count, totals[0] / count, totals[0] / count);
+
+        return makePixel(totals[0] / count, totals[1] / count, totals[2] / count);
 
     }
 
     public static int makePixel(int r, int g, int b) {
-        System.out.println("Called the make filter");
+
         return (r << 16) | (g << 8) | b;
     }
 
+    public static void mean(BufferedImage image) {
+
+        height = image.getHeight();
+        MeanFilterParallel fb = new MeanFilterParallel(image.getWidth(), 0);
+        ForkJoinPool pool = new ForkJoinPool();
+        long startTime = System.currentTimeMillis();
+        pool.invoke(fb);
+        long endTime = System.currentTimeMillis();
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        imageFile = new File("example.jpg");
+        image = ImageIO.read(imageFile);
+        image2 = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_RGB);
+        mean(image);
+        File outputfile = new File("imageBlurParallel.jpg");
+
+        // TODO- maybe just writing to the thing will work
+        ImageIO.write(image2, "jpg", outputfile);
+
+    }
 }
--- src/MedianFilterParallel.java
+++ src/MedianFilterParallel.java
@@ -1,3 +1,131 @@
-public class MedianFilterParallel {
 
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import javax.sound.midi.Sequencer.SyncMode;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.io.*;
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.*;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+
+public class MedianFilterParallel extends RecursiveAction {
+
+    static File imageFile; // args[0]
+    static BufferedImage image = null; // buffered image to send to
+    static int sliderVariable = 9; // args[2]
+    static int radius = sliderVariable / 2;
+    static BufferedImage image2 = null; // args[1]
+    protected static int sThreshold = 100;
+    int start;
+    static int height;
+    int width;
+
+    // constructor method
+    public MedianFilterParallel(int width, int start) {
+        this.width = width;
+        this.start = start;
+
+    }
+
+    protected void computeDirectly() {
+        for (int x = start; x < start + width; ++x) {
+            for (int y = 0; y < height; ++y) {
+                // call an average method
+                // if (y < 0 || y >= image.getHeight()) {
+                // continue;
+                // }
+                // System.out.println("Called the computeDirectly");
+                image2.setRGB(x, y, average(x, y, image, radius));
+            }
+
+        }
+
+        // System.out.println("Called the computeDirectly");
+    }
+
+    protected void compute() {
+        if (width < sThreshold) {
+            computeDirectly();
+
+            return;
+        } else {
+            int split = width / 2;
+
+            // split value + your start value
+            invokeAll(new MedianFilterParallel(split, start),
+                    new MedianFilterParallel(split, start + split));
+            System.out.println("Invoked the pooled threads");
+        }
+
+    }
+
+    public static int average(int x, int y, BufferedImage image, int radius) {
+        // making list arrays; maybe less dynamic
+        ArrayList<Integer> redPixels = new ArrayList<>();
+        ArrayList<Integer> bluePixels = new ArrayList<>();
+        ArrayList<Integer> greenPixels = new ArrayList<>();
+
+        int count = 0;
+        for (int i = x; i < x + radius; i++) {
+            // add outter bounds conditions
+            if (i < 0 || i >= image.getWidth()) {
+                continue;
+            }
+
+            for (int j = y - radius; j <= y + radius; ++j) {
+                if (j < 0 || j >= image.getHeight()) {
+                    continue;
+                }
+                int pixel = image.getRGB(i, j);
+                redPixels.add((pixel >> 16) & 0xff);
+                greenPixels.add((pixel >> 8) & 0xff);
+                bluePixels.add(pixel & 0xff);
+                ++count;
+
+            }
+
+        }
+        Collections.sort(redPixels);
+        Collections.sort(greenPixels);
+        Collections.sort(bluePixels);
+
+        return makePixel(redPixels.get(count / 2), greenPixels.get(count / 2), bluePixels.get(count / 2));
+
+    }
+
+    public static int makePixel(int r, int g, int b) {
+
+        return (r << 16) | (g << 8) | b;
+    }
+
+    public static void mean(BufferedImage image) {
+
+        height = image.getHeight();
+        MedianFilterParallel fb = new MedianFilterParallel(image.getWidth(), 0);
+        ForkJoinPool pool = new ForkJoinPool();
+        long startTime = System.currentTimeMillis();
+        pool.invoke(fb);
+        long endTime = System.currentTimeMillis();
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        imageFile = new File("example.jpg");
+        image = ImageIO.read(imageFile);
+        image2 = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_RGB);
+        mean(image);
+        File outputfile = new File("imageMedianParallel.jpg");
+
+        // TODO- maybe just writing to the thing will work
+        ImageIO.write(image2, "jpg", outputfile);
+
+    }
 }
